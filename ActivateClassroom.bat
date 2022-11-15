85914b79e2cea9e0c38ce688b5274edcbd846b10
astrugatch <22604593+astrugatch@users.noreply.github.com>
2022-11-15 14:57:03 -0500

    Update ActivateClassroom.bat

--- ActivateClassroom.bat
+++ ActivateClassroom.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
-
+ECHO ================================================================
+ECHO Activates a Google Classroom specified by ID
+ECHO ================================================================
 SET /P googleclass= What Classroom ID:
 
 gam update course %googleclass% status ACTIVE
